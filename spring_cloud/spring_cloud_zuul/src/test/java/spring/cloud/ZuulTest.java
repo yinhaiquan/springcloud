b40fafd215179c9e83a3080d0cf1604a737af292
yinhaiquan <yinhaiquan@juzix.io>
2018-01-09 17:12:45 +0800

    spring cloud zuul网关配置模块

---
+++ spring_cloud/spring_cloud_zuul/src/test/java/spring/cloud/ZuulTest.java
@@ -0,0 +1,23 @@
+package spring.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @title : 单元测试类
+ * @describle :
+ * <p>
+ * Create By yinhaiquan
+ * @date 2018/1/9 16:44 星期二
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZuulTest {
+
+    @Test
+    public void testZuul(){
+        System.out.println("......");
+    }
+}
